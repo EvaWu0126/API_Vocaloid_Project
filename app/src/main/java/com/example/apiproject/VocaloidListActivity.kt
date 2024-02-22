94db8bc44fb4a5d07196648ad58875f023086801
EvaWu0126 <wwq2026@gmail.com>
2024-02-22 10:03:07 -0800

    Added vocaloid item and list xmls, added mainPicture/Picture class

--- app/src/main/java/com/example/apiproject/VocaloidListActivity.kt
+++ app/src/main/java/com/example/apiproject/VocaloidListActivity.kt
@@ -7,6 +7,6 @@ import com.example.apiproject.R
 class VocaloidListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_vocaloid_list)
     }
 }
